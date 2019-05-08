30c58c73238110b6da4c4723ef90949d05651606
白龙(23119351) <bailong001@ke.com>
2019-05-08 14:08:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+https://developer.lightbend.com/guides/akka-quickstart-java/
+https://akka.io/docs/
+
+https://github.com/write2munish/Akka-Essentials
+
---
+++ src/main/java/com/kute/akka/PureakkatestApplication.java
@@ -0,0 +1,13 @@
+package com.kute.akka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureakkatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureakkatestApplication.class, args);
+	}
+
+}
